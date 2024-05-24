24f1274d3a916f618eae0fa46dce75576fdc0bed
Abhi00045 <128639147+Abhi00045@users.noreply.github.com>
2024-05-24 18:42:46 +0530

    Dark mode done

---
+++ script.js
@@ -0,0 +1,27 @@
+let toggled = document.getElementById("container");
+let ids=document.querySelector("body");
+let img=document.getElementById("image")
+let circle=document.getElementById("circle");
+let h1=document.getElementById("h1");
+
+function changed(){
+    // circle.style.transform="translateX(4.5vw)";
+    // ids.style.background="black";
+    // ids.style.color="white"
+    // circle.classList.toggle("active");
+    toggled.classList.toggle("active");
+    ids.classList.toggle("active");
+    img.classList.toggle("active");
+    h1.classList.toggle("active");
+    circle.classList.toggle("active");
+    let str="I am vengeance!";
+    h1.innerHTML=str;
+}
+// function toggle(){
+//     toggled.classList.toggle("active");
+//     let str="I am vengeance!";
+//     h1.innerHTML=str;
+// }
+toggled.addEventListener("click",changed);
+
+
